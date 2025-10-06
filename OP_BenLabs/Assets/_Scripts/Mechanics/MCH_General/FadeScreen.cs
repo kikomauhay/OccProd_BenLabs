4b0f57c50145b9495abce61dbbafe206841e8122
SF-23 <angelitojoseph.astilla@benilde.edu.ph>
2025-10-06 18:40:40 +0800

    0.16
    [FadeScreen.cs]
    - added the code from ChopChop
    - needs a Camera GameObject for the script to work (si Kiko na bahala sa setup nun)
    
    [Other]
    - added a Test Tutorial Room prefab for the graybox
    - renamed SceneHandler.cs to FloorHandler.cs

---
+++ OP_BenLabs/Assets/_Scripts/Mechanics/MCH_General/FadeScreen.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using UnityEngine;
+
+public class FadeScreen : MonoBehaviour
+{
+    #region Members
+
+    public float FadeDuration => _fadeDuration;
+
+    [Header("Debugging")]
+    [SerializeField] private Logger _logger;
+    [SerializeField] private bool _isDevMode;
+
+    [Header("Fade Settings")]
+    [SerializeField] private bool _fadeOnStart;
+    [SerializeField] private float _fadeDuration;
+    [SerializeField] private Color _fadeColor;
+
+    private Renderer _renderer;
+
+    #endregion
+
+    #region Unity
+
+    private void Awake() => _renderer = GetComponent<Renderer>();
+    private void Start()
+    {
+        Debug.Assert(_logger, "<color=red>Missing _logger reference!</color>", gameObject);
+
+        if (_isDevMode)
+            _logger.Log($"{name}'s developer mode is enabled!", gameObject, ColorType.YELLOW);
+        
+        if (_fadeOnStart) 
+            FadeIn();
+    }
+
+    #endregion
+    #region Fade
+
+    public void FadeIn() => Fade(1f, 0f);
+    public void FadeOut() => Fade(0f, 1f);
+    private void Fade(float alphaIn, float alphaOut)
+    {
+        IEnumerator CO_FadeRoutine(float alphaIn, float alphaOut)
+        {
+            float timer = 0f;
+
+            while (timer <= _fadeDuration)
+            {
+                // slowly sets the fade-in color 
+                Color newColor = _fadeColor;
+                newColor.a = Mathf.Lerp(alphaIn, alphaOut, timer / _fadeDuration);
+                _renderer.material.SetColor("_Color", newColor);
+
+                // increases opacity per frame
+                timer += Time.deltaTime;
+                yield return null;
+            }
+
+            Color col = _fadeColor;
+            col.a = alphaOut;
+            _renderer.material.SetColor("_Color", col); // resets the transparency back to normal
+            gameObject.SetActive(false);
+        }
+
+        StartCoroutine(CO_FadeRoutine(alphaIn, alphaOut));
+    }
+
+    #endregion
+}
