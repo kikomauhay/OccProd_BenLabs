bf1b60dbbdc0a51884a9a2472d5b7dc2f0bfb1d5
Factora <12210799@benilde.edu.ph>
2025-10-09 15:33:04 +0800

    0.23.1
    - adjusted floor height

--- OP_BenLabs/Assets/_Scripts/Scriptable Objects/WaveData.cs
+++ OP_BenLabs/Assets/_Scripts/Scriptable Objects/WaveData.cs
@@ -8,6 +8,7 @@ public class WaveData : ScriptableObject
     public GameObject[] Units => _units;
     public uint UnitCount => _unitCount;
     public float SpawnInterval => _spawnInterval; 
+    public float GracePeriod => _gracePeriod;
 
     #endregion
     #region SerializeField
@@ -15,6 +16,7 @@ public class WaveData : ScriptableObject
     [SerializeField] private GameObject[] _units; // prefab/s to spawn
     [SerializeField] private uint _unitCount; // how many units will spawn in the wave
     [SerializeField] private float _spawnInterval; // how frequent will they spawn
+    [SerializeField] private float _gracePeriod; // grace period before they spawn
 
     #endregion
 }
