521a1446148f654a3787d2012cc4d3ff2a5c40fb
Isagani Coel <coelfactora@gmail.com>
2025-11-17 13:31:19 +0800

    0.81.1
    - added random desktop rotation

--- OP_BenLabs/Assets/_Scripts/Misc/Desktop.cs
+++ OP_BenLabs/Assets/_Scripts/Misc/Desktop.cs
@@ -6,6 +6,9 @@ public class Desktop : MonoBehaviour
 
     #region Members
 
+    [SerializeField] private Transform _monitor;
+
+    [Header("Desktop Wallpaper")]
     [SerializeField] private Image _img;
     [SerializeField] private Sprite _specialWallpaper;
     [SerializeField] private Sprite[] _defaultWallpapers;
@@ -17,5 +20,7 @@ public class Desktop : MonoBehaviour
         _img.sprite = _specialWallpaper != null ? 
                       _specialWallpaper :
                       _defaultWallpapers[Random.Range(0, _defaultWallpapers.Length)]; 
+
+        _monitor.localRotation = Quaternion.Euler(0f, Random.Range(-10f, 10f), 0f);
     }
 }
