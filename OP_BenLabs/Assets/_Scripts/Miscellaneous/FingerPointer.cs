c47e70e536b4f650b26940630b9f17c70353a5e9
Isagani Coel <coelfactora@gmail.com>
2026-03-07 19:26:45 +0800

    1.4.10
    - fixed final actions after player dies in GDD mini-game
    - updated bottle materials to remove anything alcoholic
    
    [BUG]
    - missing voice line for whiteboard npc

---
+++ OP_BenLabs/Assets/_Scripts/Miscellaneous/FingerPointer.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class FingerPointer : Actor
+{
+    #region Properties
+        
+    #endregion
+    #region Inspector
+        
+    #endregion
+    #region Private
+        
+    #endregion
+
+    #region Actor
+        
+    #endregion
+    #region Unity
+        
+    #endregion
+    #region Public
+        
+    #endregion
+    #region private
+    
+    #region Helpers
+
+    #endregion
+    #endregion
+
+    #region Enumerators
+
+    #endregion
+}
